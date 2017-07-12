0354961fd9ff83c2f600e5c2af8c7bb8ed5e64bf
Anderson <avanegas95@gmail.com>
2017-07-11 20:44:32 -0400

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace lab7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Encrypt_Button = new System.Windows.Forms.Button();
+            this.Decrypt_Button = new System.Windows.Forms.Button();
+            this.Explorer_Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 202);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Key:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(46, 107);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(526, 31);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(57, 230);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(165, 31);
+            this.textBox2.TabIndex = 3;
+            // 
+            // Encrypt_Button
+            // 
+            this.Encrypt_Button.Location = new System.Drawing.Point(57, 316);
+            this.Encrypt_Button.Name = "Encrypt_Button";
+            this.Encrypt_Button.Size = new System.Drawing.Size(165, 52);
+            this.Encrypt_Button.TabIndex = 4;
+            this.Encrypt_Button.Text = "Encrypt";
+            this.Encrypt_Button.UseVisualStyleBackColor = true;
+            this.Encrypt_Button.Click += new System.EventHandler(this.Encrypt_Button_Click);
+            // 
+            // Decrypt_Button
+            // 
+            this.Decrypt_Button.Location = new System.Drawing.Point(331, 316);
+            this.Decrypt_Button.Name = "Decrypt_Button";
+            this.Decrypt_Button.Size = new System.Drawing.Size(162, 52);
+            this.Decrypt_Button.TabIndex = 5;
+            this.Decrypt_Button.Text = "Decrypt";
+            this.Decrypt_Button.UseVisualStyleBackColor = true;
+            this.Decrypt_Button.Click += new System.EventHandler(this.Decrypt_Button_Click);
+            // 
+            // Explorer_Button
+            // 
+            this.Explorer_Button.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Explorer_Button.BackgroundImage")));
+            this.Explorer_Button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Explorer_Button.Location = new System.Drawing.Point(610, 94);
+            this.Explorer_Button.Name = "Explorer_Button";
+            this.Explorer_Button.Size = new System.Drawing.Size(73, 70);
+            this.Explorer_Button.TabIndex = 6;
+            this.Explorer_Button.UseVisualStyleBackColor = true;
+            this.Explorer_Button.Click += new System.EventHandler(this.Explorer_Button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 407);
+            this.Controls.Add(this.Explorer_Button);
+            this.Controls.Add(this.Decrypt_Button);
+            this.Controls.Add(this.Encrypt_Button);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "File Encrypt/Decrypt";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button Encrypt_Button;
+        private System.Windows.Forms.Button Decrypt_Button;
+        private System.Windows.Forms.Button Explorer_Button;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Security;
+using System.Security.Cryptography;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace lab7
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Opens the file explorer to select a file
+        private void Explorer_Button_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.ShowDialog();
+            textBox1.Text = open.FileName;
+            SaveFileDialog save = new SaveFileDialog();
+        }
+
+        private void Encrypt_Button_Click(object sender, EventArgs e)
+        {
+            //Checks to see if the user entered a key, if not, nothing happens
+            if(textBox2.Text.Length == 0)
+            {
+                MessageBox.Show("Please enter a key", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            //Checks to see if the file we want to encrypt exists, if not, shows error message and nothing happens
+            if (!File.Exists(textBox1.Text))
+            {
+                DialogResult dialogResult = MessageBox.Show("Could not open soruce or destination file", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            FileStream Input = null;
+            FileStream Output = null;
+
+            // We try and open the input and output files, and catch exceptions
+            try
+            {
+                string file = textBox1.Text + ".des";
+                // Checks to see if the encrypted file exists and if it should be overwritten
+                if (File.Exists(file))
+                {
+                    DialogResult dialogResult = MessageBox.Show("Output file exists. Overwrite?", "File Exists", MessageBoxButtons.YesNo);
+                    if (dialogResult == DialogResult.Yes)
+                    {
+
+                    }
+                    else if (dialogResult == DialogResult.No)
+                    {
+                        Output.Close();
+                        Input.Close();
+                        return;
+                    }
+                }
+                Input = new FileStream(textBox1.Text, FileMode.Open, FileAccess.Read);
+                Output = new FileStream(textBox1.Text + ".des", FileMode.OpenOrCreate, FileAccess.Write); 
+                Output.SetLength(0);
+
+                byte[] bin = new byte[8];
+                long readlen = 0;
+                long totlen = Input.Length;
+                int len;
+
+                DES des = new DESCryptoServiceProvider();
+                byte[] desKey = new byte[8];
+                byte[] desIV;
+                int location = 0;
+
+                // Converts the characters to bytes. If longer than 8 characters, cycles through and adds subsequent characters to existing values
+                for (int i = 0; i < textBox2.Text.Length; i++)
+                {
+                    if (i <= 8)
+                    {
+                        location = i % 8;
+                        desKey[location] += (byte)textBox2.Text[i];
+                    }
+                    else
+                    {
+                        desKey[i] = (byte)textBox2.Text[i];
+                    }
+                }
+
+                desIV = desKey;
+                CryptoStream encStream = new CryptoStream(Output, des.CreateEncryptor(desKey, desIV), CryptoStreamMode.Write);
+
+                // Encrypts and writes to the file
+                while (readlen < totlen)
+                {
+                    len = Input.Read(bin, 0, 8);
+                    encStream.Write(bin, 0, 8);
+                    readlen = readlen + len;
+                }
+
+                // Closes the file and encoding
+                encStream.Close();
+                Output.Close();
+                Input.Close();
+            }
+            catch
+            {
+                // We close both files if we catch an exception
+                Output.Close();
+                Input.Close();
+            }
+            
+        }
+
+        
+
+
+
+
+        private void Decrypt_Button_Click(object sender, EventArgs e)
+        {
+            //Checks to see if the user entered a key, if not, nothing happens
+            if (textBox2.Text.Length == 0)
+            {
+                MessageBox.Show("Please enter a key", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            FileStream Input = null;
+            FileStream Output = null;
+            string file;
+            string filename = textBox1.Text;
+            int index = filename.LastIndexOf('.');
+            
+            // Get the original filename without the .des extension
+            if (index == -1)
+            {
+                file = filename;
+            }
+            else
+            {
+                file = filename.Substring(0, index);
+            }
+
+            // Check to see if the file has a .des extension. If not we show an error message
+            bool extension = Path.GetExtension(filename).Equals(".des", StringComparison.InvariantCulture);
+            if(!extension)
+            {
+                MessageBox.Show("Not a .des file.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Checks to see if our input file exists
+            if (!File.Exists(textBox1.Text))
+            {
+                DialogResult dialogResult = MessageBox.Show("Could not open soruce or destination file", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // We try opening the file and decrypting
+            try
+            {
+                Input = new FileStream(textBox1.Text, FileMode.Open, FileAccess.Read);
+                // We ask to overwrite if it exists
+                if (File.Exists(file))
+                {
+                    DialogResult dialogResult = MessageBox.Show("Output file exists. Overwrite?", "File Exists", MessageBoxButtons.YesNo);
+                    if (dialogResult == DialogResult.Yes)
+                    {
+
+                    }
+                    else if (dialogResult == DialogResult.No)
+                    {
+                        Output.Close();
+                        Input.Close();
+                        return;
+                    }
+
+                }
+                
+                Output = new FileStream(file, FileMode.OpenOrCreate, FileAccess.Write);
+                Output.SetLength(0);
+
+                byte[] bin = new byte[8];
+                long readlen = 0;
+                long totlen = Input.Length;
+                int len;
+                // Same method for encryption used for decryption
+                DES des = new DESCryptoServiceProvider();
+                byte[] desKey = new byte[8];
+                byte[] desIV;
+                int location = 0;
+                for (int i = 0; i < textBox2.Text.Length; i++)
+                {
+                    if (i <= 8)
+                    {
+                        location = i % 8;
+                        desKey[location] += (byte)textBox2.Text[i];
+                    }
+                    else
+                    {
+                        desKey[i] = (byte)textBox2.Text[i];
+                    }
+                }
+                desIV = desKey;
+                // We check to see if the key matches the file, if not we catch the exception and return an error message
+                try
+                {
+                    CryptoStream encStream = new CryptoStream(Output, des.CreateDecryptor(desKey, desIV), CryptoStreamMode.Write);
+                    while (readlen < totlen)
+                    {
+                        len = Input.Read(bin, 0, 8);
+                        encStream.Write(bin, 0, 8);
+                        readlen = readlen + len;
+                    }
+
+                    encStream.Close();
+                    Output.Close();
+                    Input.Close();
+                }
+                catch
+                {
+                    MessageBox.Show("Bad key or file.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Output.Close();
+                    Input.Close();
+                    File.Delete(file);
+                    return;
+                }      
+            }
+            catch
+            {
+                Output.Close();
+                Input.Close();
+                File.Delete(file);             
+            }
+        }
+    }
+}
